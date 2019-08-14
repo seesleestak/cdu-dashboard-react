97a5a87d38e28a52fca338e5da10fe2ba90bded8
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-08-14 10:23:25 -0700

    Remove margin from Row component in Frame

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Row, Col } from 'react-flexbox-grid'
+import React from "react";
+import { Row, Col } from "react-flexbox-grid";
 
 export default function Frame({ children }) {
   return (
-    <Row center="xs">
+    <Row center="xs" style={{ margin: 0 }}>
       <Col xs={12} sm={10} md={8} lg={6}>
         <div className="frame">{children}</div>
       </Col>

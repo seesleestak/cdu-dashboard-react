c93faf5fa240f6ff8d37bf5b8d49cc934b99e474
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-08-18 17:39:13 -0700

    Add xl breakpoint for Frame

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -4,7 +4,7 @@ import { Row, Col } from "react-flexbox-grid";
 export default function Frame({ children }) {
   return (
     <Row center="xs" style={{ margin: 0 }}>
-      <Col xs={12} sm={10} md={8} lg={6}>
+      <Col xs={12} sm={11} md={9} lg={8} xl={6}>
         <div className="frame">{children}</div>
       </Col>
     </Row>
